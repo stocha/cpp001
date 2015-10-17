a0faa9a71366dfefd6db3614d4cb77d17856f455
dfi <dfi@dfidaali.com>
2015-10-17 07:11:13 +0200

    creation

---
+++ p001/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: denis
+ *
+ * Created on 17 octobre 2015, 06:59
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "whatever";
+    return 0;
+}
+

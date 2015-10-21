eebe4b96aa85b0b8768287cfa73f6cfa114fa117
dfi <dfi@dfidaali.com>
2015-10-21 16:51:41 +0200

    testeur done

--- p001/NinXorSolv.cpp
+++ p001/NinXorSolv.cpp
@@ -7,53 +7,108 @@
 
 #include "NinXorSolv.h"
 
-void NinXorSolv::test() {
+class xorInvert : public inverterInterface {
+
+    struct myclass001 {
+
+        bool operator()(bitField a, bitField b) {
+            for (int i = 0; i < a.size(); i++) {
+                int ind = a.size() - i - 1;
+                if (a[ind] < b[ind]) return true;
+                else if (a[ind] > b[ind]) return false;
+            }
+            return true;
+        }
+    } compareBits;
+public:
+
+    vector<bitField> invert(bitField in) {
+        NinXorSolv a(in.size(), in);
+
+
+        auto res = a.sat();
+
+        sort(res.begin(), res.end(), compareBits);
+        return res;
+    }
+};
+
+void oldTest() {
     srand(0xCAFEBABE);
-    
-    int sz=16;
+
+    int sz = 16;
     bitField f(sz);
-    f.randomize();f.randomize();f.randomize();f.randomize();f.randomize();
-    f.set(3,0);
+    f.randomize();
+    f.randomize();
+    f.randomize();
+    f.randomize();
+    f.randomize();
+    f.set(3, 0);
     cout << f.str() << endl;
-    NinXorSolv a(sz,f);
-    
-    cout << a.str()<< endl;
-    
-    int un=a.unbound();
-    
-    auto b=NinXorSolv(a);
+    NinXorSolv a(sz, f);
+
+    cout << a.str() << endl;
+
+    int un = a.unbound();
+
+    auto b = NinXorSolv(a);
     cout << "forcing " << un << endl;
-    b.forceAt(un,true);
-    
-    auto c=NinXorSolv(a);
-    c.forceAt(un,false);
-    
-    cout <<"------- true" << endl;
-    cout << b.str()<< endl;
-    cout << b.strbound()<< endl;
-    
-    cout <<"------- false" << endl;
-    cout << c.str()<< endl; 
-    cout << c.strbound()<< endl; 
-    
-    auto d=NinXorSolv(a);
-    auto myx=xorExpr();
-    myx.e.push_back(prodExpr(9,8));
-    myx.e.push_back(prodExpr(7,6));
-    d.substitute(1,myx);  
-    cout << d.str()<< endl; 
-    cout << d.strbound()<< endl;    
-    
+    b.forceAt(un, true);
+
+    auto c = NinXorSolv(a);
+    c.forceAt(un, false);
+
+    cout << "------- true" << endl;
+    cout << b.str() << endl;
+    cout << b.strbound() << endl;
+
+    cout << "------- false" << endl;
+    cout << c.str() << endl;
+    cout << c.strbound() << endl;
+
+    auto d = NinXorSolv(a);
+    auto myx = xorExpr();
+    myx.e.push_back(prodExpr(9, 8));
+    myx.e.push_back(prodExpr(7, 6));
+    d.substitute(1, myx);
+    cout << d.str() << endl;
+    cout << d.strbound() << endl;
+
     cout << "-----------------" << endl;
-    vector<bitField> invr=a.sat();
-    
-    cout << invr.size() << " results found" <<endl;
-    
-    
-    for( auto b : invr){
+    vector<bitField> invr = a.sat();
+
+    cout << invr.size() << " results found" << endl;
+
+
+    for (auto b : invr) {
         cout << b.str() << endl;
     }
-    
+
+}
+
+void testCompImplxo() {
+    int nbBit = 18;
+
+    SoluSimp ss(nbBit);
+    ss.debug_coef();
+
+    //refInvert refInv;
+    seqInvert seqInv;
+    seqInvertSimetric seqSym;
+    xorInvert xoInv;
+    //compareImpl imp(&refInv,&seqInv);   
+    //compareImpl imp(&refInv,&refInv);
+    // compareImpl imp(&seqInv,&seqInv); 
+    //compareImpl imp(&seqInv, &seqSym);
+    //compareImpl imp(&xoInv, &seqSym);
+    compareImpl imp(&seqSym, &seqSym);
+    //compareImpl imp(&xoInv, &xoInv);
+    imp.compareThem(nbBit, 30);
+}
+
+void NinXorSolv::test() {
+    //oldTest();
+    testCompImplxo();
 }
 
 

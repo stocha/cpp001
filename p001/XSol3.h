47b31fedb27b6282f7534ebf78a940cda184e98b
dfi <dfi@dfidaali.com>
2015-10-25 18:09:46 +0100

    save

--- p001/XSol3.h
+++ p001/XSol3.h
@@ -30,33 +30,32 @@ namespace xsol3 {
         int left = 1;
 
         int phase = 0;
-        
-        int diagind=0;
+
+        int diagind = 0;
 
         int sz;
 
     public:
-        
-        int currloc() const{
+
+        int currloc() const {
             return vcurrloc;
         }
-        
-        int remaining() const{
+
+        int remaining() const {
             return left;
         }
-        
-        int diagnum() const{
+
+        int diagnum() const {
             return diagind;
         }
-        
-        int ptr() const{
+
+        int ptr() const {
             return vcurr;
         }
 
         cursor(int size) : sz(size) {
         }
 
-        
         long inc() {
             vcurr++;
             vcurrloc++;
@@ -70,11 +69,11 @@ namespace xsol3 {
                     cdiagsz++;
                     diagind++;
                 }
-                vcurrloc=0;
+                vcurrloc = 0;
                 left = cdiagsz;
             }
 
-            return vcurr^(sz*sz+sz*2);
+            return vcurr^(sz * sz + sz * 2);
         }
 
         bool operator++() {
@@ -84,14 +83,39 @@ namespace xsol3 {
         string str() const {
             std::ostringstream sout;
 
-            
-           // sout << " [ Dsz " << cdiagsz << " cloc " << vcurrloc << " PH " << phase << " left " << left << " ] ";
-             sout << "(" << diagnum() << "|" <<  vcurrloc  ;
-            
-            
+
+            // sout << " [ Dsz " << cdiagsz << " cloc " << vcurrloc << " PH " << phase << " left " << left << " ] ";
+            sout << "(" << diagnum() << "|" << vcurrloc;
+
+
             return sout.str();
         }
 
+
+    };
+
+    class coefdiag {
+        int sz;
+    public:
+
+        coefdiag(int size) : sz(size) {
+        }
+
+        int coefL(int at, int num) {
+            return (at - num);
+        }
+
+        int coefLsec(int at, int num) {
+            return sz / 2 - 1 + num - at;
+        }
+
+        int coefR(int at, int num) {
+            return num + sz / 2;
+        }
+
+        int coefRsec(int at, int num) {
+            return sz / 2 - 1 - num + sz / 2;
+        }
 
     };
 
@@ -102,32 +126,66 @@ namespace xsol3 {
         int sz;
 
     private:
-        
-        int at(cursor const c ) {
+
+        int at(cursor const c) {
             return one[c.ptr()];
         }
+
+        void setAt(cursor const c, int const x, int const y) {
+            one[c.ptr()] = vp(x, y);
+        }
+
     public:
 
         equation(vector<bool> in) : sz(in.size()), one(in.size()), dat(in.size()*(in.size() + 1) + 1) {
             cursor c(sz);
-            
-            do{
-                // cout << c.str() << " | " ;
-                
-                int v=at(c);
-                cout << "[" << ((v<<16) & 0xFFFF) << "." << (v&0xFFFF) << "]" ;
-                if(c.remaining()==0){
-                    cout<< endl;
-                }                
-               
+            coefdiag coef(sz);
 
-                
-            }while(++c);
+            do {
+                // cout << c.str() << " | " ;
+
+                int v = at(c);
+
+                if (c.currloc()) {
+                    int x = coef.coefL(c.currloc() - 1, c.diagnum() / 2);
+                    int y = coef.coefR(c.currloc() - 1, c.diagnum() / 2);
+                    // coef.coefR(c.currloc(),c.diagnum()/2);
+                    //  coef.coefR(c.diagnum()/2, c.currloc());
+
+                    cout << c.str() << " | " << "(" << x << "," << y << ")";
+                    setAt(c, x, y);
+
+                    if (c.remaining() == 0) {
+
+                        cout << endl;
+                    }
+
+                }
+
+
+
+
+            } while (++c);
         }
-        
-        string str(){
+
+        string str() {
             std::ostringstream sout;
-            
+            cursor c(sz);
+
+
+            do {
+                // cout << c.str() << " | " ;
+
+                int v = at(c);
+                sout << "[" << ((v << 16) & 0xFFFF) << "." << (v & 0xFFFF) << "]";
+                if (c.remaining() == 0) {
+                    sout << endl;
+                }
+
+
+
+            } while (++c);
+
             return sout.str();
         }
 
@@ -135,20 +193,19 @@ namespace xsol3 {
 
     class XSol3 {
         int sz;
-        
+
     public:
-        XSol3(int size ) : sz(size){}
-        
-        
-        
+
+        XSol3(int size) : sz(size) {
+        }
 
         void debugParcours(vector<bool> in) {
-            
+
             equation e(in);
-            
+
             cout << e.str() << endl;
 
-        }        
+        }
     private:
 
     };
